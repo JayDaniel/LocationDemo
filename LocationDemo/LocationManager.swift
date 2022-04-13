44352b5a0f19331df6ab9305c4a4249ecbaa7123
JayDaniel <9743878+JayDaniel@users.noreply.github.com>
2022-04-13 13:38:14 +0800

    * 优化计算方法。

--- LocationDemo/LocationManager.swift
+++ LocationDemo/LocationManager.swift
@@ -13,13 +13,19 @@ enum LocationError: Error {
     case showTipsError(reason: String? = nil)
 }
 
+enum DistanceMethod {
+    /// 根据CLLocation计算
+    case firstVersion
+    /// 根据坐标点计算
+    case secondVersion
+    /// 根据公式计算
+    case thirdVersion
+}
+
 class LocationManager: NSObject {
-    
     static let manager = LocationManager()
-    
     /// 默认比对gps坐标
     var currLocation = LocationModel(lat: "31.22629", lon: "121.63948")
-    
     // gps数据源
     var locationList: [LocationModel] = []
     
@@ -27,7 +33,9 @@ class LocationManager: NSObject {
     /// - Parameters:
     ///   - resourcePath: 资源路径
     ///   - polyline: 与资源文件做对比的坐标model
-    func startParser(resourcePath: String, location: LocationModel? = nil) async throws -> LocationModel {
+    func startParser(method: DistanceMethod,
+                     resourcePath: String,
+                     location: LocationModel? = nil) async throws -> LocationModel {
         
         if let location = location {
             currLocation = location
@@ -40,7 +48,7 @@ class LocationManager: NSObject {
         //开始解析
         parser?.parse()
 
-        return try await getNearestPoint()
+        return try await getNearestPoint(method)
     }
     
     /// 解析转换字典为model
@@ -61,36 +69,20 @@ class LocationManager: NSObject {
         }
     }
     
-    /// 计算当前两点距离
-    /// - Parameters:
-    ///   - firstPoint: 开始点位
-    ///   - lastPoint: 结束点位
-    func distanceBetweenOrderBy(distancePoint: LocationModel) -> Double {
-        //第一个坐标
-        let current = CLLocation(latitude: currLocation.useLat, longitude: currLocation.useLon)
-        //第二个坐标
-        let before = CLLocation(latitude: distancePoint.useLat, longitude: distancePoint.useLon)
-        // 计算距离
-        return current.distance(from: before)
-    }
-    
     /// 获取比对后的最近点
     /// - Returns: 最近点model
-    func getNearestPoint() async throws -> LocationModel {
-        guard var meterModel = locationList.first else {
+    func getNearestPoint(_ method: DistanceMethod) async throws -> LocationModel {
+        guard let meterModel = locationList.first else {
             throw LocationError.showTipsError(reason: "解析异常，坐标集合为空")
         }
-        var comparisonNum: Double = distanceBetweenOrderBy(distancePoint: meterModel)
-        // 循环比对出最近点
-        locationList.forEach { polylineModel in
-            let meter = distanceBetweenOrderBy(distancePoint: polylineModel)
-            if meter < comparisonNum {
-                comparisonNum = meter
-                meterModel = polylineModel
-            }
+        switch method {
+        case .firstVersion:
+            return getDistanceWithFirstVersion(distancePoint: meterModel)
+        case .secondVersion:
+            return getDistanceWithSecondVersion(distancePoint: meterModel)
+        case .thirdVersion:
+            return getDistanceWithThirdVersion(distancePoint: meterModel)
         }
-        print("两点之间最短距离===\(comparisonNum)")
-        return meterModel
     }
 }
 
@@ -118,5 +110,93 @@ extension LocationManager: XMLParserDelegate {
     /// - Parameter parser:
     func parserDidEndDocument(_ parser: XMLParser) {
         print("标签已经解析完成===")
+    }
+}
+
+//MARK: - 直接根据经纬度计算两点直线距离
+extension LocationManager {
+    
+    /// 根据 CLLocation获取两个经纬度直线距离
+    /// - Parameter distancePoint:
+    /// - Returns: 距离值
+    private func getLocationDifference(distancePoint: LocationModel) -> Double {
+        //第一个坐标
+        let current = CLLocation(latitude: currLocation.useLat, longitude: currLocation.useLon)
+        //第二个坐标
+        let before = CLLocation(latitude: distancePoint.useLat, longitude: distancePoint.useLon)
+        // 计算距离
+        return current.distance(from: before)
+    }
+    
+    /// 计算出与指定坐标直线最近的坐标
+    /// - Parameter distancePoint: 当前起始坐标
+    /// - Returns: 最近坐标
+    func getDistanceWithFirstVersion(distancePoint: LocationModel) -> LocationModel {
+        var distanceNum = getLocationDifference(distancePoint: distancePoint)
+        var coordinateModel = distancePoint
+        locationList.forEach { polylineModel in
+            let meter = getLocationDifference(distancePoint: polylineModel)
+            if meter < distanceNum {
+                distanceNum = meter
+                coordinateModel = polylineModel
+            }
+        }
+        return coordinateModel
+    }
+}
+
+//MARK: - 根据两点经纬度计算两点距离
+extension LocationManager {
+    //根据角度计算弧度
+    private func radian(d: Double) -> Double {
+        return d * Double.pi / 180.0
+    }
+    
+    private func getDistanceDifference(distancePoint: LocationModel) -> Double {
+        let EARTH_RADIUS: Double = 6378137.0
+        
+        let radLat1: Double = self.radian(d: currLocation.useLat)
+        let radLat2: Double = self.radian(d: distancePoint.useLat)
+          
+        let radLng1: Double = self.radian(d: currLocation.useLon)
+        let radLng2: Double = self.radian(d: distancePoint.useLon)
+          
+        let a: Double = radLat1 - radLat2
+        let b: Double = radLng1 - radLng2
+          
+        var s: Double = 2 * asin(sqrt(pow(sin(a/2), 2) + cos(radLat1) * cos(radLat2) * pow(sin(b/2), 2)))
+        s = s * EARTH_RADIUS
+        
+        return s
+    }
+    
+    //根据两点经纬度计算两点距离
+    func getDistanceWithSecondVersion(distancePoint: LocationModel) -> LocationModel {
+        var distanceNum = getDistanceDifference(distancePoint: distancePoint)
+        var coordinateModel = distancePoint
+        locationList.forEach { polylineModel in
+            let meter = getDistanceDifference(distancePoint: polylineModel)
+            if meter < distanceNum {
+                distanceNum = meter
+                coordinateModel = polylineModel
+            }
+        }
+        return coordinateModel
+    }
+}
+//MARK: - 数学公式计算
+extension LocationManager {
+    
+    func getDistanceWithThirdVersion(distancePoint: LocationModel) -> LocationModel {
+        var distanceNum = 0.0;
+        var coordinateModel = distancePoint
+        for (index, element) in locationList.enumerated(){
+            let tmpDistance = fabs(sqrt(pow(element.useLat - currLocation.useLat, 2) + pow(element.useLon - currLocation.useLon, 2)))
+            if index == 0 || tmpDistance < distanceNum {
+                distanceNum = tmpDistance
+                coordinateModel = element
+            }
+        }
+        return coordinateModel
     }
 }
--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -20,12 +20,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
         Task {
             do {
-                let nearPoint = try await LocationManager.manager.startParser(resourcePath: resourcePath, location: currPolyline)
-                print("最短距离坐标===\(nearPoint)")
+                let nearPoint1 = try await LocationManager.manager.startParser(method: .firstVersion,
+                                                                              resourcePath: resourcePath,
+                                                                              location: currPolyline)
+                
+                let nearPoint2 = try await LocationManager.manager.startParser(method: .secondVersion,
+                                                                              resourcePath: resourcePath,
+                                                                              location: currPolyline)
+                
+                let nearPoint3 = try await LocationManager.manager.startParser(method: .thirdVersion,
+                                                                              resourcePath: resourcePath,
+                                                                              location: currPolyline)
+                print("最短距离坐标===\(nearPoint1)")
+                print("最短距离坐标===\(nearPoint2)")
+                print("最短距离坐标===\(nearPoint3)")
+                
             } catch let error as LocationError {
                 switch error{
                 case .showTipsError(reason: let msg) :
